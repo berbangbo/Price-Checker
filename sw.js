51a491fb889ffea1e1ac82a9f895aea10548e6ba
berbangbo <basxxlllx@gmail.com>
2026-08-26 19:35:07 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE = 'price-finder-v8';
-const SHELL = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest'];
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL))));
-self.addEventListener('fetch', event => { if (event.request.method !== 'GET') return; event.respondWith(caches.match(event.request).then(found => found || fetch(event.request))); });
